be944c0eea8fc7216c737a2ba7b3d38aa8d47dbb
Alexandre Lima <contato@alexandrelima.dev>
2020-07-26 10:12:47 -0300

    148. Primeira classe - Position

--- ws-eclipse/curso_programacao/src/s16/chess_system/application/Program.java
+++ ws-eclipse/curso_programacao/src/s16/chess_system/application/Program.java
@@ -1,10 +1,14 @@
 package s16.chess_system.application;
 
+import s16.chess_system.boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 		
 	}
 
