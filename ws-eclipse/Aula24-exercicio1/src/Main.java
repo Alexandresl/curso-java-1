3bc1a8a4f26a36c7d91f55e515adba5407d54e59
Alexandre Lima <al3xandr3sl@gmail.com>
2021-08-23 14:43:53 -0300

    24. Saída de dados em Java

--- ws-eclipse/Aula24-exercicio1/src/Main.java
+++ ws-eclipse/Aula24-exercicio1/src/Main.java
@@ -25,7 +25,6 @@ public class Main {
 		System.out.printf("Rouded (three decimal places): %.3f\n", measure);
 		Locale.setDefault(Locale.US);
 		System.out.printf("US decimal point: %.3f\n", measure);
-		
 
 	}
 
