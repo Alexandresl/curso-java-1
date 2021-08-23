756892be55fec2d624cea27eab8421f102980e1f
Alexandre Lima <al3xandr3sl@gmail.com>
2021-08-23 17:05:14 -0300

    25. Processamento de dados em Java, Casting

---
+++ ws-eclipse/Aula25-Exemplo2/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x;
+		double y;
+		
+		x = 5;
+		y = 2 * 5;
+		
+		System.out.println(x); // 5
+		System.out.println(y); // 10.0
+
+	}
+
+}
---
+++ ws-eclipse/Aula25-Exemplo5/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+
+	}
+
+}
