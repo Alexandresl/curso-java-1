4f898c7852a98faea7a88003093243d03e076c4a
Alexandre Lima <al3xandr3sl@gmail.com>
2021-08-24 11:43:37 -0300

    35. Estrutura condicional (if-else)

---
+++ ws-eclipse/Aula35-Exemplo3/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int hora;
+				
+		System.out.print("Que horas são? ");
+		hora = sc.nextInt();
+		
+		if (hora < 12) {
+			System.out.println("Bom dia!");
+		} else {
+			if (hora < 18) {
+				System.out.println("Boa tarde!");
+			} else {
+				System.out.println("Boa noite!");
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
