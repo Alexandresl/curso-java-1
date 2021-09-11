5238becdfee55d33efec06f3533dde3293623c52
Alexandre Lima <al3xandr3sl@gmail.com>
2021-09-11 00:10:01 -0300

    66. Criando um método para obtermos os benefícios de reaproveitamento e delegação

---
+++ ws-eclipse/Aula066-Exemplo1/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
+		String larger = (areaX > areaY) ? "X" : "Y";
+		System.out.print("Larger area: " + larger);
+		
+		sc.close();
+
+	}
+
+}
