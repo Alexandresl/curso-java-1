5d74323aed73dd99196cf5154ecf107d5e8f7ba2
Alexandre Lima <al3xandr3sl@gmail.com>
2021-09-19 13:04:44 -0300

    Mensagem de boa noite

--- ws-eclipse/Aula112-Exemplo-01/src/application/Program.java
+++ ws-eclipse/Aula112-Exemplo-01/src/application/Program.java
@@ -1,12 +1,11 @@
 package application;
 
 public class Program {
-
 	public static void main(String[] args) {
 		
 		System.out.println("Bom dia");
 		System.out.println("Boa tarde");
-
+		System.out.println("Boa noite");
+		
 	}
-
 }
