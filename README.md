fffc70e039e136d41d24edc2ce6dda4d532e0908
Alexandre Lima <al3xandr3sl@gmail.com>
2021-09-18 21:31:47 -0300

    ### 100. Exercício proposto

--- README.md
+++ README.md
@@ -1807,3 +1807,23 @@ Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salá
 Fazer um programa para ler um número inteiro N e uma matriz de ordem N contendo números inteiros. Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz.
 
 [Exemplo 1](ws-eclipse\Aula099-Exemplo01)
+
+### 100. Exercício proposto
+
+Fazer um programa para ler dois números inteiros M e N, e depois ler uma matriz de M linhas por N colunas contendo números inteiros, podendo haver repetições. Em seguida, ler um número inteiros X que pertence à matriz. Para cada ocorrência de X, mostrar os valores à esquerda, acima, à direita e abaixo de X, quando houver, conforme exemplo:
+
+*3 4*
+*10 8 15 12*
+*21 11 23 8*
+*14 5 13 19*
+*8*
+*Position 0,1:*
+*Left: 10*
+*Right: 15*
+*Down: 11*
+*Position 1,3:*
+*Left: 23*
+*Up: 12*
+*Down: 19*
+
+[Exercício 1](ws-eclipse\Aula100-Exemplo01)
---
+++ ws-eclipse/Aula100-Exemplo01/src/application/Program.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		int[][] mat = new int[m][n];
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		int x = sc.nextInt();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j);
+					if (j - 1 >= 0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					
+					if (j + 1 < n) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					
+					if (i - 1 >= 0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					
+					
+					if (i + 1 < m) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+					
+					
+				}
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
